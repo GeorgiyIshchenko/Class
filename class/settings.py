aac6186a9dc8d07507f60f3d1de0e008d437ce80
GeorgeIshchenko <gogaigd2005@gmail.com>
2020-11-07 18:47:05 +0300

    Changes

--- class/settings.py
+++ class/settings.py
@@ -126,3 +126,5 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR,'static_in_dev')]
--- class/urls.py
+++ class/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('',include('my_class.urls')),
 ]
 
-
+if settings.DEBUG==True:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
